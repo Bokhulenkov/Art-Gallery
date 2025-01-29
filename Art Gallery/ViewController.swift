3e1f25d817d3141a70a452054422306142d3c79d
https://github.com/bokhulenkov/ <bokhulenkov@gmail.com>
2025-01-29 18:01:09 +0300

    Initial Commit

---
+++ Art Gallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Art Gallery
+//
+//  Created by Alexander Bokhulenkov on 29.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
